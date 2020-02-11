df06165f5920d2a0c33bcf17c14b9436b6d23f8e
Julien Capellari <julien@capellari.net>
2020-02-11 15:20:17 +0100

    update: config

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 // Environment
 const env = {
-  JWT_KEY: process.env.JWT_KEY || '',
+  JWT_KEY: process.env.JWT_KEY || 'superkey!',
   MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/lucifer",
 
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8000,
   PRODUCTION: process.env.NODE_ENV === 'production'
 };
 
