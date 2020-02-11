d4859afc7632419b2af1cecd57a94973776c26fd
Julien Capellari <julien@capellari.net>
2020-02-11 01:17:06 +0100

    add: some middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import express from 'express';
 
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 // Application
 const app = express();
+
+// Options
+app.set('trust poxy', true);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 export default app;
