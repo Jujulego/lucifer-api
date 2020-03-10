6801d8b23025415df1208195523e7cd26f07f852
Julien Capellari <julien.capellari@neoxia.com>
2020-03-10 16:57:17 +0100

    add: room management

--- src/wsapp.ts
+++ src/wsapp.ts
@@ -2,9 +2,12 @@ import { Server } from 'http';
 import SocketIO from 'socket.io';
 
 import { wsauth } from 'middlewares/auth';
+import { HttpError } from 'middlewares/errors';
 
 import Daemons from 'controllers/daemons';
 import Users from 'controllers/users';
+
+import { isAllowed, PLvl } from 'data/permission';
 
 // App
 async function wsapp(server: Server) {
@@ -17,6 +20,52 @@ async function wsapp(server: Server) {
 
   // Middlewares
   io.use(wsauth);
+
+  // Events
+  io.on('connection', (sock) => {
+    try {
+      // Personal room
+      sock.user().then(user => sock.join(user.id));
+
+      // Events
+      sock.on('register', async (room) => {
+        // Get current user
+        const user = await sock.user();
+
+        // Try to join room
+        switch (room) {
+          case 'users':
+          case 'daemons':
+            if (!isAllowed(user, room, PLvl.READ)) {
+              throw HttpError.Forbidden();
+            }
+            sock.join(room);
+
+            break;
+
+          default:
+            throw HttpError.NotFound(`Unknown room: ${room}`);
+        }
+      });
+
+      sock.on('unregister', (room) => {
+        // Leave room
+        switch (room) {
+          case 'users':
+          case 'daemons':
+            sock.leave(room);
+            break;
+
+          default:
+            throw HttpError.NotFound(`Unknown room: ${room}`);
+        }
+      });
+
+    } catch (error) {
+      console.error(error);
+      sock.error(error);
+    }
+  });
 }
 
 export default wsapp;
