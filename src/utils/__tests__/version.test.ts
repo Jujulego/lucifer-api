b4f8373963bba76975dfa3b17c1636c4bcb9ca7a
Julien Capellari <julien@capellari.net>
2020-03-23 09:56:10 +0100

    fix: version test

--- src/utils/__tests__/version.test.ts
+++ src/utils/__tests__/version.test.ts
@@ -1,8 +1,8 @@
 import { version } from '../version';
 
 describe('utils/version', () => {
-  test('version', () => {
-    const v = version();
+  test('version', async () => {
+    const v = await version();
 
     expect(v).toHaveProperty('version');
     expect(v).toHaveProperty('commit');
