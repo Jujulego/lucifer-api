708c0bcfe9507ddc0a6fe4d3f3f44ee2153bc240
Julien Capellari <julien@capellari.net>
2020-03-25 14:32:49 +0100

    add: test user creation

--- src/data/user/user.test.ts
+++ src/data/user/user.test.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
+import { User } from './user';
+import UserModel from './user.model';
 import UserRepository from './user.repository';
 
 // Tests
@@ -15,17 +17,58 @@ describe('data/user', () => {
     });
   });
 
+  // Fill database
+  let users: User[] = [];
+
+  beforeEach(async () => {
+    // Create some users
+    users = await Promise.all([
+      new UserModel({ email: 'test1@test.com', password: 'test1' }).save(),
+      new UserModel({ email: 'test2@test.com', password: 'test2' }).save(),
+      new UserModel({ email: 'test3@test.com', password: 'test3' }).save(),
+      new UserModel({ email: 'test4@test.com', password: 'test4' }).save(),
+    ]);
+  });
+
+  // Empty database
+  afterEach(async () => {
+    // Delete created users
+    await Promise.all(users.map(user => user.remove()));
+  });
+
+  // Disconnect
   afterAll(async () => {
     await mongoose.disconnect();
   });
 
   // Tests
-  test('UserRepository.create', async () => {
+  test('UserRepository.create: new user', async () => {
     const repo = new UserRepository();
     const user = await repo.create({ email: 'test@test.com', password: 'test' });
 
     expect(user._id).toBeDefined();
     expect(user.email).toEqual('test@test.com');
     expect(await bcrypt.compare('test', user.password)).toBeTruthy();
+
+    // Delete created user
+    await user.remove();
+  });
+
+  test('UserRepository.create: email case', async () => {
+    const repo = new UserRepository();
+    const user = await repo.create({ email: 'TeST@tESt.coM', password: 'test' });
+
+    expect(user._id).toBeDefined();
+    expect(user.email).toEqual('test@test.com');
+    expect(await bcrypt.compare('test', user.password)).toBeTruthy();
+
+    // Delete created user
+    await user.remove();
+  });
+
+  test('UserRepository.create: existing user', async () => {
+    const repo = new UserRepository();
+
+    await expect(repo.create({ email: 'test1@test.com', password: 'test1' })).rejects.toThrow();
   });
 });
