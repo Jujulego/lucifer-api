36e91be51014203febef03349aac613a4fc86b8b
Julien Capellari <julien@capellari.net>
2020-03-30 19:29:34 +0200

    update: move auth declarations to declaration files

---
+++ src/types/express.d.ts
@@ -0,0 +1,8 @@
+declare namespace Express {
+  // noinspection JSUnusedGlobalSymbols
+  interface Request {
+    daemon?: import('data/daemon/daemon').Daemon;
+    token?: import('data/token/token').Token;
+    user?: import('data/user/user').User;
+  }
+}
---
+++ src/types/socket.d.ts
@@ -0,0 +1,7 @@
+declare namespace SocketIO {
+  // noinspection JSUnusedGlobalSymbols
+  interface Socket {
+    user: () => Promise<import('data/user/user').User>;
+    token: () => Promise<import('data/token/token').Token>;
+  }
+}
