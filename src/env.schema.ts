c00a51a073d62ba0cfe010a8a87eee2a60db423d
Julien Capellari <julien@capellari.net>
2020-06-06 00:05:54 +0200

    update: validate environment

---
+++ src/env.schema.ts
@@ -0,0 +1,22 @@
+import joi from '@hapi/joi';
+
+// Schema
+export const envSchema = joi.object({
+  AUTH_STRATEGY: joi.allow('auth0', 'jwt')
+    .default('auth0'),
+
+  PORT: joi.number()
+    .port().default(8000),
+
+  AUTH0_DOMAIN: joi.string()
+    .domain().required(),
+
+  AUTH0_AUDIENCE: joi.string()
+    .required(),
+
+  AUTH0_CLIENT_ID: joi.string()
+    .required(),
+
+  AUTH0_CLIENT_SECRET: joi.string()
+    .required()
+});
--- src/env.ts
+++ src/env.ts
@@ -1,30 +1,31 @@
+import { Logger } from '@nestjs/common';
 import dotenv from 'dotenv';
 
-// Constants
-const STRATEGIES = ['auth0', 'jwt']
+import { envSchema } from 'env.schema';
 
 // Load .env file
 dotenv.config();
 
-// Check for missing vars
-if (!process.env.AUTH0_DOMAIN)        throw new Error('Missing AUTH0_DOMAIN variable');
-if (!process.env.AUTH0_CLIENT_ID)     throw new Error('Missing AUTH0_CLIENT_ID variable');
-if (!process.env.AUTH0_CLIENT_SECRET) throw new Error('Missing AUTH0_CLIENT_SECRET variable');
+// Validate env
+const result = envSchema.validate(process.env, { allowUnknown: true });
 
-if (process.env.AUTH_STRATEGY && STRATEGIES.indexOf(process.env.AUTH_STRATEGY) === -1) {
-  throw new Error(`Invalid AUTH_STRATEGY: unknown strategy ${process.env.AUTH_STRATEGY}`);
+if (result.error) {
+  const logger = new Logger('Configuration')
+  logger.error(result.error.message);
+  process.exit(1);
 }
 
 // Environment
 export const env = {
-  AUTH_STRATEGY: process.env.AUTH_STRATEGY || 'auth0',
+  AUTH_STRATEGY: process.env.AUTH_STRATEGY,
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT!,
   PRODUCTION: process.env.NODE_ENV === 'production',
   TESTS: process.env.NODE_ENV === 'test',
 
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN!,
+  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE!,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID!,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET!
 };
+
