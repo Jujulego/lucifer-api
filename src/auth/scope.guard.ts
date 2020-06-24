555104740b3acfbdacdfab9a166f630593adf7ef
Julien Capellari <julien@capellari.net>
2020-06-24 17:47:57 +0200

    add: scope guard

---
+++ src/auth/scope.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
+
+import { Token } from './token.model';
+import { Reflector } from '@nestjs/core';
+
+// Decorator
+export const Scopes = (...scopes: string[]) => SetMetadata('scopes', scopes);
+
+// Guard
+@Injectable()
+export class ScopeGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector
+  ) {}
+
+  // Methods
+  canActivate(ctx: ExecutionContext): boolean {
+    // Get scopes
+    const scopes = this.reflector.get<string[]>('scopes', ctx.getHandler());
+    if (!scopes || scopes.length === 0) return true;
+
+    // Get token
+    const request = ctx.switchToHttp().getRequest();
+    const token = request.user as Token;
+    if (!token) return false;
+
+    // Match
+    return scopes.every(scope => token.permissions.includes(scope));
+  }
+}
