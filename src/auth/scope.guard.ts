a646d264042b057fa53869f361f063ffd5585c67
Julien Capellari <julien@capellari.net>
2020-06-25 10:34:24 +0200

    add: allow scope override

--- src/auth/scope.guard.ts
+++ src/auth/scope.guard.ts
@@ -1,10 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
-
-import { Token } from './token.model';
 import { Reflector } from '@nestjs/core';
 
-// Decorator
+import { Token } from './token.model';
+
+// Types
+export type AllowIfCallback<R = any> = (req: R, token: Token) => boolean;
+
+// Decorators
 export const Scopes = (...scopes: string[]) => SetMetadata('scopes', scopes);
+export const AllowIf = <R = any> (cb: AllowIfCallback<R>) => SetMetadata('scopes:allow', cb);
 
 // Guard
 @Injectable()
@@ -15,8 +19,10 @@ export class ScopeGuard implements CanActivate {
 
   // Methods
   canActivate(ctx: ExecutionContext): boolean {
-    // Get scopes
+    // Get metadata
     const scopes = this.reflector.get<string[]>('scopes', ctx.getHandler());
+    const allow = this.reflector.get<AllowIfCallback>('scopes:allow', ctx.getHandler());
+
     if (!scopes || scopes.length === 0) return true;
 
     // Get token
@@ -25,6 +31,7 @@ export class ScopeGuard implements CanActivate {
     if (!token || !token.permissions) return false;
 
     // Match
+    if (allow && allow(request, token)) return true;
     return scopes.every(scope => token.permissions.includes(scope));
   }
 }
